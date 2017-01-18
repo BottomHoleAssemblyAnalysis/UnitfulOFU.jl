e67bb9032786c14f06e9c87edbc4ce90de4388bf
Rob J Goedman <goedman@mac.com>
2017-01-18 09:37:31 -0800

    Oops, forgot runtests.jl itself.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,16 @@
-using UnitfulOFU
+using BHAAnalysis
 using Base.Test
 
-@testset "OFU string macro" begin
-    @test macroexpand(:(ofu"lbf")) == :(UnitfulUS.ustrcheck(UnitfulOFU.lbf_ofu))
-    @test macroexpand(:(ofu"1.0")) == 1.0
-    @test macroexpand(:(ofu"rpm")) == :(UnitfulUS.ustrcheck(UnitfulOFU.rpm_ofu)
-    @test macroexpand(:(ofu"1.0hp")) ==
-        :(1.0 * UnitfulOFU.ustrcheck(UnitfulUS.hp_us))
+# write your own tests here
 
-    @test isa(macroexpand(:(ofu"hp rpm")).args[1], ParseError)
+println("Running tests for UnitfulOFU:")
 
-    # Disallowed functions
-    @test isa(macroexpand(:(ofu"abs(2)")).args[1], ErrorException)
+tests = [
+  "ofu_test.jl"
+]
 
-    # Units not found
-    @test isa(macroexpand(:(ofu"kg")).args[1], ErrorException)
-
-    # test ustrcheck(x) fallback to catch non-units / quantities
-    @test isa(macroexpand(:(ofu"ustrcheck")).args[1], ErrorException)
+for test in tests
+    println("\n  * $(test) *\n")
+    include(test)
+    println()
 end
