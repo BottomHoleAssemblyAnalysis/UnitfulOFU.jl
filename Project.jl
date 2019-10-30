5c965a8bbb0bca1ba39158e3563c189252e406cf
Rob J Goedman <goedman@mac.com>
2019-10-30 14:23:44 -0600

    Added Project.toml

---
+++ Project.jl
@@ -0,0 +1,19 @@
+name = "UnitfulOFU"
+uuid = "c38b2286-e06d-5aaf-ac8c-46cde5afb720"
+license = "MIT"
+desc = "Oil Field Units extension to Unitful"
+authors = ["Rob J Goedman <goedman@icloud.com>"]
+version = "0.0.1"
+
+[deps]
+Unitful = "1986cc42-f94f-5a68-af5c-568840ba703d"
+
+[compat]
+Unitful = "^0"
+julia = "^1"
+
+[extras]
+Test = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
+
+[targets]
+test = ["Test"]
